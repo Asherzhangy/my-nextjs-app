19a8508c5a40f48c0a1d29747542e161ea7add57
AsherZhang <114404153+Asherzhangy@users.noreply.github.com>
2025-06-19 08:58:24 +0800

    ログイン要求応答関数の処理 Handle login request response function

--- src/app/admin/login/page.tsx
+++ src/app/admin/login/page.tsx
@@ -1,12 +1,19 @@
 'use client';
 import { Card, Form, Button, Input } from 'antd';
-
+import { useRouter } from 'next/navigation';
 function LoginPage() {
+  const nav = useRouter();
   return (
     <div className='login-form pt-20 mx-auto w-full max-w-md'>
       <Card title='Next全栈管理后台' className='w-4/5 mx-auto mt-20'>
-        <Form labelCol={{span:3}} onFinish={(v)=>{
-            console.log(v)
+        <Form labelCol={{span:3}} onFinish={ async (v)=>{
+            // console.log(v)
+            const res = await fetch('/api/admin/login',{
+              method:'POST',
+              body:JSON.stringify(v),
+            }).then((res)=>res.json());
+            console.log(res)
+            nav.push('/admin/dashboard');
         }}>
         <Form.Item name="userName" label='用户名'>
           <Input placeholder='请输入用户名' />
