4d89b8e8ad563e8c0c0927b26b6160b6b3963f4b
AsherZhang <114404153+Asherzhangy@users.noreply.github.com>
2025-06-19 08:35:20 +0800

    ミドルウェアを使用して管理者バックグラウンドのログイン傍受機能を完成させる Use middleware to complete the login interception function of the admin background

---
+++ src/app/admin/_components/PageContainer.tsx
@@ -0,0 +1,8 @@
+'use client';
+import { React } from "react";
+
+function pageContainer() {
+    return <div >pageContainer</div>;
+}
+
+export default pageContainer;
---
+++ src/app/admin/dashboard/page.tsx
@@ -0,0 +1,9 @@
+import { React } from "react";
+import pageContainer from "../_components/PageContainer";
+function dashboardPage() {
+    return (<pageContainer title="dashboard">
+        <h3>here is code</h3>
+    </pageContainer>);
+}
+
+export default dashboardPage;
